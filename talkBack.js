88469e15251bf0741ab74634391d1571eb2eecfe
Jared Wallace <jared-wallace@us.ibm.com>
2017-04-15 09:11:59 -0500

    Formatting

--- talkBack.js
+++ talkBack.js
@@ -139,8 +139,7 @@ module.exports.talkback = function (data, token, url, space) {
           msg += "*ID: " +
             res.results[x].id +
             "* - " +
-            res.results[x].subject +
-            ' \n';
+            res.results[x].subject;
         }
         ww.sendMessage(msg, '#016F4A', url, space, token);
         // We need to check next page

f8db79260498d7f059a619b63f1092e8cc33c8e5
Jason V <vegas3416@gmail.com>
2017-04-19 18:13:50 +0000

    zendesk master copy

--- zendesk.js
+++ zendesk.js
@@ -3,20 +3,20 @@ var ww = require("./lib/ww");
 
 const ZENDESK_URL = "https://ibmworkspace.zendesk.com/api/v2/";
 
-module.exports.handleTrigger = function (body, request, url, space, token) {
-  console.log("Body from Zen POST: " + JSON.stringify(body));
+module.exports.handleTrigger = function (body, res, url, space, token) {
+  console.log("Entered zendesk.handleTrigger");
+  res.status(200).end();
   var msg = "";
   var color = "";
   // New ticket was created
   if (body.create) {
-    msg = body.id +
-          "\n" +
-          body.title +
-          "\n*URL: *" +
-          body.url +
-          "\n" +
-          body.info;
-    if(msg.indexOf('ibm') > -1) {
+    console.log("Zendesk notified us of a newly created ticket");
+    msg = body.id + "\n" + body.title + "\n*URL: *" + body.url +
+          "\n" + body.info;
+    // This needs to be refactored to check against a list of target email
+    // domains. This will give us better visibility of tickets from important
+    // companies.
+    if(msg.includes("ibm")) {
       color = 'green';
     }
     else {
@@ -25,30 +25,26 @@ module.exports.handleTrigger = function (body, request, url, space, token) {
   }
   // Existing ticket was updated
   else if (body.update) {
-    console.log("I got into update");
-    msg = body.id +
-          "\n" +
-          body.title +
-          "\n*Assigned To: *" +
-          body.assigned +
-          "\n*Latest request came from: *" +
-          body.requester +
-          "\n*URL: *" +
-          body.url +
-          "\n" +
-          body.info;
+    console.log("Zendesk notified us of an updated ticket");
+    msg = body.id + "\n" + body.title + "\n*Assigned To: *" + body.assigned +
+          "\n*Latest request came from: *" + body.requester + "\n*URL: *" +
+          body.url + "\n" + body.info;
+    // Probably ought to include a similar check to what we have for new tickets
+    // - checking against certain email domains.
     color = 'yellow';
+  } else {
+    console.log("Unexpected callback from Zendesk");
+    console.log("Body from Zendesk was: " + JSON.stringify(body));
+    return;
   }
-
-  console.log("\nZendesk trigger event became: " + msg);
 
   ww.sendMessage(msg, color, url, space, token);
 
 };
 
 module.exports.callZendesk = function (type, callback) {
+  console.log("Entered zendesk.callZendesk.");
   var options = {
-    //url appends type to zendeks URL to be used for what is sent to Zen
     url: ZENDESK_URL + type,
     'auth': {
       'user': process.env.Z_USER,
@@ -56,20 +52,19 @@ module.exports.callZendesk = function (type, callback) {
       'Accept': "application/json"
     }
   };
-  console.log("Performing Zendesk API call with " + JSON.stringify(options));
+  console.log("Performing Zendesk API call of type: " + type);
   request(options, function (err, res, body) {
     if (err) {
-      //callback is a function(err,res) and the reason why null is there b/c we are only looking at the err part of it
+      console.log("Zendesk API call failed.");
       callback(err, null);
     } else {
-      console.log ("Got zendesk api calls");
-      //console.log("Zendesk API call succeeded, with result: " + body);
+      console.log("Zendesk API call succeeded.");
       callback(null, JSON.parse(body));
     }
   });
 };
 
-///update ticket from zendesk
+//update ticket test
 module.exports.updateZendesk = function (type, callback) {
   var options = {
     //url appends type to zendeks URL to be used for what is sent to Zen
